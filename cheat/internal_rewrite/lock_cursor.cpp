5314e1d0e8506e86e02b0623932660ebb32d2277
navewindre <nw@moneybot.cc>
2021-06-18 15:45:32 +0200

    .

---
+++ cheat/internal_rewrite/lock_cursor.cpp
@@ -0,0 +1,10 @@
+#include "hooks.hpp"
+
+void __fastcall hooks::lock_cursor( void* ecx_, void* edx_ ) {
+	if( g_settings.menu.open || g_con->m_open ) {
+		g_csgo.m_surface( )->UnlockCursor( );
+		return;
+	}
+
+	lock_cursor_o( ecx_, 0 );
+}
